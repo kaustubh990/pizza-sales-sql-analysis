6b862346152f5f59f4014e395f76140412d08e0f
kaustubh990 <l1u9ckyy@gmail.com>
2026-06-11 23:11:24 +0530

    Update advance qs.sql

--- advance qs.sql
+++ advance qs.sql
@@ -37,3 +37,38 @@ JOIN pizzas
     ON orders_details.pizza_id = pizzas.pizza_id
 GROUP BY orders.order_date
 ORDER BY orders.order_date;
+
+
+
+
+--qs3
+WITH pizza_revenue AS (
+    SELECT
+        pizza_types.category,
+        pizza_types.name AS pizza_type,
+        SUM(orders_details.quantity * pizzas.price) AS revenue
+    FROM orders_details
+    JOIN pizzas
+        ON orders_details.pizza_id = pizzas.pizza_id
+    JOIN pizza_types
+        ON pizzas.pizza_type_id = pizza_types.pizza_type_id
+    GROUP BY pizza_types.category, pizza_types.name
+),
+ranked_pizzas AS (
+    SELECT
+        category,
+        pizza_type,
+        revenue,
+        RANK() OVER (
+            PARTITION BY category
+            ORDER BY revenue DESC
+        ) AS rank_num
+    FROM pizza_revenue
+)
+SELECT
+    category,
+    pizza_type,
+    revenue
+FROM ranked_pizzas
+WHERE rank_num <= 3
+ORDER BY category, revenue DESC;
